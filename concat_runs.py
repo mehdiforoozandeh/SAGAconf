a8a38c5305975de69c0f4137793b1c5ade4cf187
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2021-12-05 11:05:14 -0800

    ...

--- concat_runs.py
+++ concat_runs.py
@@ -10,14 +10,14 @@ from datetime import datetime
 
 genome_data_files = ['batch_runs/rep1.genomedata', 'batch_runs/rep2.genomedata']
 
-paramdict_1 = {"name_sig_main":"concat_11", "name_sig_aux":"concat_12",
+paramdict_1 = {"name_sig_main":"batch_runs/concat_11", "name_sig_aux":"batch_runs/concat_12",
     "random_seed":73, "include":'encodePilotRegions.hg19.bed', "track_weight":float(1e-2/12),
     "stws":1e-2, "ruler_scale":1000, "prior_strength":1, "resolution":1000,
     "num_labels":16, "traindir":'batch_runs/train_dir_1', "mini_batch_fraction":0.1,
     "genomedata_file_main":genome_data_files[0],  "genomedata_file_aux":genome_data_files[1],
     "posteriordir_main":"batch_runs/posterior_concat_11",  "posteriordir_aux":"batch_runs/posterior_concat_12"}
 
-paramdict_2 = {"name_sig_main":"concat_21", "name_sig_aux":"concat_22",
+paramdict_2 = {"name_sig_main":"batch_runs/concat_21", "name_sig_aux":"batch_runs/concat_22",
     "random_seed":73, "include":'encodePilotRegions.hg19.bed', "track_weight":float(1e-2/12),
     "stws":1e-2, "ruler_scale":1000, "prior_strength":1, "resolution":1000,
     "num_labels":16, "traindir":'batch_runs/train_dir_2', "mini_batch_fraction":0.1,
@@ -27,7 +27,7 @@ paramdict_2 = {"name_sig_main":"concat_21", "name_sig_aux":"concat_22",
 segway_concatenated_and_postprocess(paramdict_1)
 segway_concatenated_and_postprocess(paramdict_2)
 
-parse_posterior_results("batch_runs/posterior_concat_11", 'batch_runs/encodePilotRegions.hg19.bed', 1000, M=50)
-parse_posterior_results("batch_runs/posterior_concat_12", 'batch_runs/encodePilotRegions.hg19.bed', 1000, M=50)
-parse_posterior_results("batch_runs/posterior_concat_21", 'batch_runs/encodePilotRegions.hg19.bed', 1000, M=50)
-parse_posterior_results("batch_runs/posterior_concat_22", 'batch_runs/encodePilotRegions.hg19.bed', 1000, M=50)
+parse_posterior_results("batch_runs/concat_11", 'batch_runs/encodePilotRegions.hg19.bed', 1000, M=50)
+parse_posterior_results("batch_runs/concat_12", 'batch_runs/encodePilotRegions.hg19.bed', 1000, M=50)
+parse_posterior_results("batch_runs/concat_21", 'batch_runs/encodePilotRegions.hg19.bed', 1000, M=50)
+parse_posterior_results("batch_runs/concat_22", 'batch_runs/encodePilotRegions.hg19.bed', 1000, M=50)
