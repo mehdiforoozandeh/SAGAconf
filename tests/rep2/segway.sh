53485a8c1ddedf9c2b7ab3f2e960d6498983fd43
Mehdi Foroozandeh <mehdiforoozandehsh@gmail.com>
2022-03-26 14:44:01 -0700

    Delete tests directory

--- tests/rep2/segway.sh
+++
@@ -1,4 +0,0 @@
-## segway 3.0.3 run 49036ba0228d11ec970c7cd30ac65aea at 2021-10-01 00:57:55.315177
-
-cd "/project/6014832/mforooz/segwayconf/GM12878/new_runs"
-"/home/mforooz/miniconda3/envs/segenv/bin/segway" "train" "--include-coords=encodePilotRegions.hg19.bed" "--num-instances=5" "--track-weight=0.0008333333333333333" "--segtransition-weight-scale=0.01" "--ruler-scale=1000" "--prior-strength=1" "--resolution=1000" "--num-labels=16" "rep2.genomedata" "train_1_rep2"
