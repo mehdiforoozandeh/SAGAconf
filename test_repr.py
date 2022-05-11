62506330a509894d727aa0e105fe70a7ee327770
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2022-05-11 16:54:15 -0700

    ...

--- test_repr.py
+++ test_repr.py
@@ -242,10 +242,6 @@ if __name__=="__main__":
     # replicate_2_dir = 'tests/segway_runs/gm12878_rep2/parsed_posterior_short.csv'
     # test_calibration(replicate_1_dir, replicate_2_dir)
     
-    RUN_ALL_REPROD_ANALYSIS(
-        "segway_runs",  ['K562', 'MCF-7', 'GM12878', 'HeLa-S3', 'CD14-positive monocyte'], "testdir", type="segway", n_processors=4)
-    exit()
-    
     replicate_1_dir = 'tests/segway_runs/GM12878_rep1/'
     replicate_2_dir = 'tests/segway_runs/GM12878_rep2/'
     # benchmark(replicate_1_dir, replicate_2_dir,"tests/reprod_segw")
