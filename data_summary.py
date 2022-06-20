da22fdad84c1bca04ec268680d968ea08fc4792a
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2022-06-20 13:47:50 -0700

    ...

--- data_summary.py
+++ data_summary.py
@@ -10,21 +10,38 @@ def get_summary(download_dir):
             tfmd = pd.read_csv(download_dir+"/"+ct+'/'+tr+'/track_files_metadata.csv')
             tfmd.index = list(tfmd['Unnamed: 0'])
             tfmd = tfmd.drop('Unnamed: 0', axis=1)
+
+            # summary.append(
+            #     [
+            #         ct, tfmd.loc["assay", "rep1_alig"], 
+            #         str(", ".join([
+            #             tfmd.loc["biosample", "rep1_alig"]+" (rep. 1)", 
+            #             tfmd.loc["biosample", "rep2_alig"]+" (rep. 2)"
+            #             ])),
+            #         str(", ".join([
+            #             tfmd.loc["accession", "rep1_alig"], tfmd.loc["accession", "rep1_fcoc"], 
+            #             tfmd.loc["accession", "rep2_alig"], tfmd.loc["accession", "rep2_fcoc"]
+            #             ]))
+            #     ]
+            # )
+
             summary.append(
                 [
-                    ct, tfmd.loc["assay", "rep1_alig"], 
-                    str(", ".join([
-                        tfmd.loc["biosample", "rep1_alig"]+" (rep. 1)", 
-                        tfmd.loc["biosample", "rep2_alig"]+" (rep. 2)"
-                        ])),
-                    str(", ".join([
-                        tfmd.loc["accession", "rep1_alig"], tfmd.loc["accession", "rep1_fcoc"], 
+                    ct, tfmd.loc["assay", "rep1_alig"], tfmd.loc["biosample", "rep1_alig"]+" (rep. 1)", str(", ".join([
+                        tfmd.loc["accession", "rep1_alig"], tfmd.loc["accession", "rep1_fcoc"]
+                    ]))
+                ]
+            )
+            summary.append(
+                [
+                    ct, tfmd.loc["assay", "rep2_alig"], tfmd.loc["biosample", "rep2_alig"]+" (rep. 2)", str(", ".join([
                         tfmd.loc["accession", "rep2_alig"], tfmd.loc["accession", "rep2_fcoc"]
-                        ]))
+                    ]))
                 ]
             )
     summary = pd.DataFrame(summary, columns=['Celltype', 'experiment', 'biosample (isoreplicate)', 'files'])
     return summary
+
 
 if __name__ == "__main__":
     download_dir = sys.argv[1]
