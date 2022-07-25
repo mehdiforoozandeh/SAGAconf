9839c771614e34a2064e854d7b578d4a449366fa
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2022-07-25 10:14:10 -0700

    ...

--- run.py
+++ run.py
@@ -1260,11 +1260,11 @@ def run_single_reprod_analysis(input_dict):
 
     with open(input_dict["output_dir"]+"/run_info.txt", 'w') as fw:
         fw.write(str(input_dict))
-    # try:
-    full_reproducibility_report(input_dict["rep1_dir"], input_dict["rep2_dir"], input_dict["output_dir"], run_on_subset=True)
-    # except:
-    #     print("failed to run long_{}".format(input_dict["runtype"]))
-    #     pass
+    try:
+        full_reproducibility_report(input_dict["rep1_dir"], input_dict["rep2_dir"], input_dict["output_dir"], run_on_subset=True)
+    except:
+        print("failed to run long_{}".format(input_dict["runtype"]))
+        pass
     
 def run_single_short_report(input_dict):
     print("running type: {}".format(input_dict["runtype"]))
