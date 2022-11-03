979a50d68f83d8d365bd3ce364fd799bfdc92df6
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2022-11-03 14:05:50 -0700

    new mnemonics model

---
+++ indepth.py
@@ -0,0 +1,27 @@
+def length_vs_boundary():
+    pass
+
+def compare_signal_dist():
+    pass
+
+def sigdist_vs_overlap():
+    pass
+
+def get_all_celltype():
+    pass
+
+def get_all_label():
+    pass
+
+if __name__=="__main__":
+    rep1 = "tests/DEBUGGING/CHMM/MCF7_CONCAT_R1"
+    rep2 = "tests/DEBUGGING/CHMM/MCF7_CONCAT_R2"
+
+    rep1 = "tests/DEBUGGING/CHMM/MCF7_R1"
+    rep2 = "tests/DEBUGGING/CHMM/MCF7_R2"
+
+    rep1 = "tests/DEBUGGING/SEGWAY/MCF7_CONCAT_R1"
+    rep2 = "tests/DEBUGGING/SEGWAY/MCF7_CONCAT_R2"
+
+    rep1 = "tests/DEBUGGING/SEGWAY/MCF7_R1"
+    rep2 = "tests/DEBUGGING/SEGWAY/MCF7_R2"
