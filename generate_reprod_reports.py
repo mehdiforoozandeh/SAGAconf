09dbed7ff6b66ba00cd473579506fa04d6b68dac
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2023-01-16 22:59:03 -0800

    ...

--- generate_reprod_reports.py
+++ generate_reprod_reports.py
@@ -8,65 +8,65 @@ def r1vsr2():
         replicate_1_dir="chromhmm_runs/GM12878_rep1/", 
         replicate_2_dir="chromhmm_runs/GM12878_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/GM12878/preferred_default_ENCFF240WBI.tsv")
 
     GET_ALL(
         replicate_1_dir="segway_runs/GM12878_rep1/", 
         replicate_2_dir="segway_runs/GM12878_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/GM12878/preferred_default_ENCFF240WBI.tsv")
             
         ######## MCF-7 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/MCF-7_rep1/", 
         replicate_2_dir="chromhmm_runs/MCF-7_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/MCF-7/geneExp_dict_ENCFF721BRA.pkl")
 
     GET_ALL(
         replicate_1_dir="segway_runs/MCF-7_rep1/", 
         replicate_2_dir="segway_runs/MCF-7_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/MCF-7/geneExp_dict_ENCFF721BRA.pkl")
 
         ######## CD14 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/CD14-positive_monocyte_rep1/", 
         replicate_2_dir="chromhmm_runs/CD14-positive_monocyte_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
 
     GET_ALL(
         replicate_1_dir="segway_runs/CD14-positive_monocyte_rep1/", 
         replicate_2_dir="segway_runs/CD14-positive_monocyte_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
         
         ######## K562 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/K562_rep1/", 
         replicate_2_dir="chromhmm_runs/K562_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/K562/preferred_default_ENCFF840UYD.tsv")
 
     GET_ALL(
         replicate_1_dir="segway_runs/K562_rep1/", 
         replicate_2_dir="segway_runs/K562_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/K562/preferred_default_ENCFF840UYD.tsv")
 
         ######## HeLa-S3 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/HeLa-S3_rep1/", 
         replicate_2_dir="chromhmm_runs/HeLa-S3_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
 
     GET_ALL(
         replicate_1_dir="segway_runs/HeLa-S3_rep1/", 
         replicate_2_dir="segway_runs/HeLa-S3_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
 
 def concat():
     ################### concatenated ###################
@@ -76,65 +76,65 @@ def concat():
         replicate_1_dir="chromhmm_runs/GM12878_concat/", 
         replicate_2_dir="chromhmm_runs/GM12878_concat/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/GM12878/preferred_default_ENCFF240WBI.tsv")
 
     GET_ALL(
         replicate_1_dir="segway_runs/GM12878_concat_rep1/", 
         replicate_2_dir="segway_runs/GM12878_concat_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/GM12878/preferred_default_ENCFF240WBI.tsv")
             
         ######## MCF-7 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/MCF-7_concat/", 
         replicate_2_dir="chromhmm_runs/MCF-7_concat/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/MCF-7/geneExp_dict_ENCFF721BRA.pkl")
 
     GET_ALL(
         replicate_1_dir="segway_runs/MCF-7_concat_rep1/", 
         replicate_2_dir="segway_runs/MCF-7_concat_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/MCF-7/geneExp_dict_ENCFF721BRA.pkl")
 
         ######## CD14 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/CD14-positive_monocyte_concat/", 
         replicate_2_dir="chromhmm_runs/CD14-positive_monocyte_concat/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
 
     GET_ALL(
         replicate_1_dir="segway_runs/CD14-positive_monocyte_concat_rep1/", 
         replicate_2_dir="segway_runs/CD14-positive_monocyte_concat_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
         
         ######## K562 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/K562_concat/", 
         replicate_2_dir="chromhmm_runs/K562_concat/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/K562/preferred_default_ENCFF840UYD.tsv")
 
     GET_ALL(
         replicate_1_dir="segway_runs/K562_concat_rep1/", 
         replicate_2_dir="segway_runs/K562_concat_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/K562/preferred_default_ENCFF840UYD.tsv")
 
         ######## HeLa-S3 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/HeLa-S3_concat/", 
         replicate_2_dir="chromhmm_runs/HeLa-S3_concat/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
 
     GET_ALL(
         replicate_1_dir="segway_runs/HeLa-S3_concat_rep1/", 
         replicate_2_dir="segway_runs/HeLa-S3_concat_rep2/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
 
 def paraminit():
     ################### param - init ###################
@@ -144,62 +144,62 @@ def paraminit():
         replicate_1_dir="chromhmm_runs/GM12878_rep1_rs5/", 
         replicate_2_dir="chromhmm_runs/GM12878_rep1_rs27/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/GM12878/preferred_default_ENCFF240WBI.tsv")
 
     GET_ALL(
         replicate_1_dir="segway_runs/GM12878_rep1_rs5/", 
         replicate_2_dir="segway_runs/GM12878_rep1_rs7/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/GM12878/preferred_default_ENCFF240WBI.tsv")
             
         ######## MCF-7 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/MCF-7_rep1_rs5/", 
         replicate_2_dir="chromhmm_runs/MCF-7_rep1_rs27/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/MCF-7/geneExp_dict_ENCFF721BRA.pkl")
 
     GET_ALL(
         replicate_1_dir="segway_runs/MCF-7_rep1_rs5/", 
         replicate_2_dir="segway_runs/MCF-7_rep1_rs7/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/MCF-7/geneExp_dict_ENCFF721BRA.pkl")
 
         ######## CD14 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/CD14-positive_monocyte_rep1_rs5/", 
         replicate_2_dir="chromhmm_runs/CD14-positive_monocyte_rep1_rs27/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
 
     GET_ALL(
         replicate_1_dir="segway_runs/CD14-positive_monocyte_rep1_rs5/", 
         replicate_2_dir="segway_runs/CD14-positive_monocyte_rep1_rs7/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
         
         ######## K562 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/K562_rep1_rs5/", 
         replicate_2_dir="chromhmm_runs/K562_rep1_rs27/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/K562/preferred_default_ENCFF840UYD.tsv")
 
     GET_ALL(
         replicate_1_dir="segway_runs/K562_rep1_rs5/", 
         replicate_2_dir="segway_runs/K562_rep1_rs7/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq="biovalidation/RNA_seq/K562/preferred_default_ENCFF840UYD.tsv")
 
         ######## HeLa-S3 ########
     GET_ALL(
         replicate_1_dir="chromhmm_runs/HeLa-S3_rep1_rs5/", 
         replicate_2_dir="chromhmm_runs/HeLa-S3_rep1_rs27/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
 
     GET_ALL(
         replicate_1_dir="segway_runs/HeLa-S3_rep1_rs5/", 
         replicate_2_dir="segway_runs/HeLa-S3_rep1_rs7/", 
         genecode_dir="biovalidation/parsed_genecode_data_hg38_release42.csv", 
-        rnaseq="")
+        rnaseq=None)
