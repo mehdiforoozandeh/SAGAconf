f86482f639ba4824dbb2343df9a38f4ade5b8057
mehdiforoozandeh <mehdiforoozandehsh@gmail.com>
2025-07-28 12:07:34 -0700

    ...

--- SAGAconf.py
+++ SAGAconf.py
@@ -250,30 +250,30 @@ else:
             print("Failed to generated sample analysis.")
 
 
-    try:
-        loci1, loci2 = load_data(posterior1_dir, posterior2_dir, subset=issubset, logit_transform=False, force_WG=False)
-        loci1, loci2 = process_data(
-            loci1, loci2, replicate_1_dir, replicate_2_dir, mnemons=mnem, bm=args.base_mnemonics, 
-            vm=args.verif_mnemonics, match=False, custom_order=True)
+    # try:
+    #     loci1, loci2 = load_data(posterior1_dir, posterior2_dir, subset=issubset, logit_transform=False, force_WG=False)
+    #     loci1, loci2 = process_data(
+    #         loci1, loci2, replicate_1_dir, replicate_2_dir, mnemons=mnem, bm=args.base_mnemonics, 
+    #         vm=args.verif_mnemonics, match=False, custom_order=True)
 
-        post_clustering(loci1, loci2, args.savedir, locis=True, to=args.iou_threshold, tr=args.repr_threshold)
+    #     post_clustering(loci1, loci2, args.savedir, locis=True, to=args.iou_threshold, tr=args.repr_threshold)
 
-    except:
-        if args.verbosity:
-            print("Failed to perform post-clustering")
+    # except:
+    #     if args.verbosity:
+    #         print("Failed to perform post-clustering")
 
-    # if args.merge_clusters !=-1:
-    #     try:
-    #         loci1, loci2 = load_data(posterior1_dir, posterior2_dir, subset=issubset, logit_transform=False, force_WG=False)
-    #         loci1, loci2 = process_data(
-    #             loci1, loci2, replicate_1_dir, replicate_2_dir, mnemons=mnem, bm=args.base_mnemonics, 
-    #             vm=args.verif_mnemonics, match=False, custom_order=True)
+    if args.merge_clusters !=-1:
+        try:
+            loci1, loci2 = load_data(posterior1_dir, posterior2_dir, subset=issubset, logit_transform=False, force_WG=False)
+            loci1, loci2 = process_data(
+                loci1, loci2, replicate_1_dir, replicate_2_dir, mnemons=mnem, bm=args.base_mnemonics, 
+                vm=args.verif_mnemonics, match=False, custom_order=True)
 
-    #         post_clustering_keep_k_states(loci1, loci2, args.savedir, k=args.merge_clusters, locis=True, write_csv=False)
+            post_clustering_keep_k_states(loci1, loci2, args.savedir, k=args.merge_clusters, locis=True, write_csv=False)
 
-    #     except:
-    #         if args.verbosity:
-    #             print("Failed to merge clusters up to k")
+        except:
+            if args.verbosity:
+                print("Failed to merge clusters up to k")
         
     try:
         loci1, loci2 = load_data(posterior1_dir, posterior2_dir, subset=issubset, logit_transform=True, force_WG=False)
